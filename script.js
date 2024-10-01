18ed2ae36a90aae5e90cb2cd2908796b45c27e04
Infant22 <rojarinfant@gmail.com>
2024-10-01 19:35:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var typed = new Typed ("#typed", {
+    strings: ['Software Developer.', 'FrontEnd Developer.', 'Pega Developer.'], 
+    typeSpeed: 70,
+    backSpeed: 25,
+    loop: true
+});
+
+var senddetails = document.querySelector(".send")
+senddetails.addEventListener("Click",function(){
+    alert("Your Message has been sent Successfully")
+})
+
+
+
+
+
+
+
+
+
+
+
+ 
